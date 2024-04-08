b0696edfa2ef4902e6ee71ed492b4024a7a35401
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-04-08 11:38:10 -0300

    mat 1

---
+++ ex_type_script/003-typecasting.js
@@ -0,0 +1,5 @@
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+var resultado2 = multiplicar(2, 3);
+console.log(resultado2);
---
+++ ex_type_script/003-typecasting.ts
@@ -0,0 +1,8 @@
+function multiplicar(num1: number, num2: number){
+    return num1 * num2;
+}
+
+const resultado2 = multiplicar(2,3) as number;
+
+console.log(resultado2);
+
